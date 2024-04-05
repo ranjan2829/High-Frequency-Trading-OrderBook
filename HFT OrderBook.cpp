169695e185bd77561638fb309b4d53f16b710ccf
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-05 23:36:06 +0530

    HFT ORDER BOOK

--- HFT OrderBook.cpp
+++ HFT OrderBook.cpp
@@ -18,10 +18,43 @@
 #include <optional>
 #include <tuple>
 #include <format>
+enum class OrderType{
+    GoodTillCancel,
+    FillAndKill
+};
+
+enum class Side{
+    Buy,
+    Sell
+};
+using Price =std::int32_t;
+using Quantity =std::uint32_t;
+using OrderId=std::uint64_t;
+struct LevelInfo{
+    Price price_;
+    Quantity quantity_;
+};
+using LevelInfos=std::vector<LevelInfo>;
+class OrderBookLevelInfos{
+    public:
+    OrderBookLevelInfos(const LevelInfos& bids,const LevelInfos& ask)
+         : bids_{bids}
+         , asks_{asks}
+    {}
+    const LevelInfos& GetBids() const {return bids_;}
+    const LevelInfos& GetBids() const {return asks_;}
+    private:
+    LevelInfos bids_;
+    LevelInfos asks_;
+    
+
+
+}
+
 
 int main(){
 
 
     return 0;
-    
+
 }
