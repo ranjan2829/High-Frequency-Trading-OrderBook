99899f44f061810337a8ad7ec011ede2f6432c63
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-06 15:46:03 +0530

    HFT ORDER BOOK

--- HFT OrderBook.cpp
+++ HFT OrderBook.cpp
@@ -85,6 +85,22 @@ class Order{
 }
 using OrderPointer =std::shared_ptr<Order>;
 using OrderPointers=std::list<OrderPointer>;
+class OrderModify{
+    public :
+    OrderModify(OrderId orderID,Side side,Price price,Quantity quantity)
+            :orderId_{orderId}
+            ,price_{price}
+            ,side_{side}
+            ,quantity_ {quantity}
+
+    {}
+    OrderId GetOrderId() const { return orderId_;}
+    Price GetPrice() const {return price_;}
+    Side GetSide() const {return side_;}
+    Quantity GetQuantity() const {return quantity_;}
+
+    OrderPointer ToOrderPointer(OrderType)
+}
 
 
 
