d65853c27193935821e5d5c8aacacdee02a49e99
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-07 16:24:48 +0530

    HFT OrderBook

--- HFT OrderBook.cpp
+++ HFT OrderBook.cpp
@@ -285,7 +285,22 @@ public:
         }
         std::size_t Size() const { return orders_.size();}
         OrderbookLevelInfos GetOrderInfos()  const{
-            LevelInfos
+            LevelInfos bidInfos ,askInfos;
+            bidInfos.reserve(orders_.size());
+            askInfos.reserve(order_.size());
+            auto CreateLevelInfos=[](Price ,const OrderPointers& orders){
+                return LevelInfos{price ,std::accumulate(orders.begin(),orders.end(),(Quantity)0,
+                [](std::size_t runningSum,const OrderPointer& order)
+                {return runningSum + order->GetRemainingQuantity();})};
+
+            };
+            for(const auto& [price,orders]:bids_){
+                bidInfos.push_back(CreateLevelInfos(price,orders));
+
+            }
+            for(const auto& [price,orders]:asks_){
+                askInfos.push_back(CreateLevelInfos(price,orders));
+            }
         }
 
     }
