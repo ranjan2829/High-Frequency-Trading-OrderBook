5d13bcbcec97743ee34d9f17d4e7e7ec949bac20
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-07 23:25:44 +0530

    HFT OrderBook

--- Order.h
+++ Order.h
@@ -1,2 +1,20 @@
-#pragma once 
+#pragma once
 #include <list>
+#include <exception
+#include <format>
+#include "OrderType.h"
+#include "Side.h"
+#include "Usings.h"
+#include "Constants.h"
+
+class Order{
+public:
+    Order(OrderType orderType,OrderId orderID,Side side,Price price,Quantity quantity)
+         :orderType_{orderType}
+         ,orderId_{orderId}
+         ,side_{side}
+         ,price_{price}
+         ,initialQuantity_{quantity}
+         ,remainingQuantity_{quantity}
+    {}
+};
