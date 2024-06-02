3b36aee4efb77838a3cd4be7ab2ef87d4ec606a7
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-03 00:59:16 +0530

    Done

--- HFT-OrderBook.cpp
+++ HFT-OrderBook.cpp
@@ -365,13 +365,4 @@ public:
   }
 };
 
-int main() {
-  OrderBook orderbook;
-  const OrderId orderId = 1;
-  orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId,
-                                             Side::Buy, 100, 10));
-  std::cout << orderbook.Size() << std::endl;
-  orderbook.CancelOrder(orderId);
-  std::cout << orderbook.Size() << std::endl;
-  return 0;
-}
+
