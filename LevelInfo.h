196f00b2e122ef9639412471b2e12b276c155fd4
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-03 00:52:16 +0530

    Done

--- LevelInfo.h
+++ LevelInfo.h
@@ -2,3 +2,11 @@
 // Created by Ranjan shahaji shitole on 03/06/24.
 //
 
+#pragma once
+#include "Usings.h"
+struct LevelInfo{
+    Price price_;
+    Quantity quantity_;
+
+};
+using LevelInfos=std::vector<LevelInfo>;
