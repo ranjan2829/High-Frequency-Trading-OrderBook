211c11475a164546cd5bc4129ecdebf445810e97
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-03 00:22:49 +0530

    Done

--- Usings.h
+++ Usings.h
@@ -2,3 +2,10 @@
 // Created by Ranjan shahaji shitole on 02/06/24.
 //
 
+#pragma once
+#include <vector>
+
+using Price =std::int32_t;
+using Quantity =std::uint32_t;
+using OrderId=std::uint64_t;
+using OrderIds=std::vector<OrderId>
