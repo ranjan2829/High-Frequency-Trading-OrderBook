9add3eda2c8b5007f7a2ca735d5e19ac6d831b3e
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-03 00:21:10 +0530

    Done

--- TradeInfo.h
+++ TradeInfo.h
@@ -2,7 +2,13 @@
 // Created by Ranjan shahaji shitole on 03/06/24.
 //
 
-#ifndef ORDERBOOK_MARKET_TRADEINFO_H
-#define ORDERBOOK_MARKET_TRADEINFO_H
+#pragma once
 
-#endif //ORDERBOOK_MARKET_TRADEINFO_H
+#include "Usings.h"
+
+struct TradeInfo
+{
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
+};
--- Usings.h
+++ Usings.h
@@ -2,7 +2,3 @@
 // Created by Ranjan shahaji shitole on 02/06/24.
 //
 
-#ifndef ORDERBOOK_MARKET_USINGS_H
-#define ORDERBOOK_MARKET_USINGS_H
-
-#endif //ORDERBOOK_MARKET_USINGS_H
