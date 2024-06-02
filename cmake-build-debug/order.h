2ab48832cc450984393c830fc804b5f0602cf1d9
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-02 14:10:38 +0530

    Done

---
+++ cmake-build-debug/order.h
@@ -0,0 +1,12 @@
+//
+// Created by Ranjan shahaji shitole on 02/06/24.
+//
+
+#pragma once
+enum class OrderType{
+    GoodtillCancel,
+    FillAndKill,
+    FillOrKill,
+    GoodForDay,
+    Market,
+};
