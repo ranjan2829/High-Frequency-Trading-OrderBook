5b76b916cc5473b114ae14a4fdea0b7f5457ec14
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-06 00:18:22 +0530

    Done

---
+++ OrderType.h
@@ -0,0 +1,14 @@
+//
+// Created by Ranjan shahaji shitole on 06/06/24.
+//
+
+#pragma once
+
+enum class OrderType
+{
+    GoodTillCancel,
+    FillAndKill,
+    FillOrKill,
+    GoodForDay,
+    Market,
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-//
-// Created by Ranjan shahaji shitole on 03/06/24.
-//
-#include "OrderBook.h"
+#include <iostream>
+#include <memory>
+#include "OrderBook.h"  // Make sure this header defines OrderBook, OrderId, Order, OrderType, Side, etc.
 
 int main() {
     OrderBook orderbook;
     const OrderId orderId = 1;
-    orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId,
-                                               Side::Buy, 100, 10));
+    orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderId, Side::Buy, 100, 10));
     std::cout << orderbook.Size() << std::endl;
     orderbook.CancelOrder(orderId);
     std::cout << orderbook.Size() << std::endl;
